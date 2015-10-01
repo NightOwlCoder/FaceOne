1f010048f5dd52bfdcd3f04834db83edefa68991
Night Owl <NightOwlCoder@users.noreply.github.com>
2015-10-01 13:55:44 -0700

    Working version, 1 symbol

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
 #include <pebble.h>
+
+  static Window *s_main_window;
+  static TextLayer *s_time_layer;
+  static TextLayer *s_weather_layer;
+  static TextLayer *s_weather_city_layer;
+  static TextLayer *s_symbol_layer;
+  static TextLayer *s_symbol_price_layer;
   
-static Window *s_main_window;
-static TextLayer *s_time_layer;
-static TextLayer *s_weather_layer;
-static TextLayer *s_weather_city_layer;
-static TextLayer *s_symbol_layer;
-static TextLayer *s_symbol_price_layer;
-
-static GFont s_time_font;
-
+  static GFont s_time_font;
+  
 enum {
   KEY_TEMPERATURE = 0,
   KEY_CONDITIONS = 1,
@@ -31,7 +31,7 @@ static void update_time() {
     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
   else
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  
+
   text_layer_set_text(s_time_layer, buffer);
 }
 
@@ -44,7 +44,7 @@ static void fetch_weather()
   int value = 1; 
   dict_write_int(iter, 1, &value, sizeof(int), true); 
   dict_write_end(iter); 
- 
+
   // Send the message!
   app_message_outbox_send();
 }
@@ -53,13 +53,13 @@ static void main_window_load(Window *window) {
 
   //Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LCD_35));
-  
+
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(4, 0, 140, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_font(s_time_layer, s_time_font);
-  
+
   // Create symbol TextLayer
   s_symbol_layer = text_layer_create(GRect(4, 45, 140, 50));
   text_layer_set_background_color(s_symbol_layer, GColorClear);
@@ -73,7 +73,7 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_weather_layer, GTextAlignmentRight);
   text_layer_set_font(s_weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_text(s_weather_layer, "guessing...");
-  
+
   // Create Weather City Layer
   s_weather_city_layer = text_layer_create(GRect(0, 147, 140, 35));
   text_layer_set_background_color(  s_weather_city_layer, GColorClear);
@@ -94,7 +94,7 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   //Unload GFont
   fonts_unload_custom_font(s_time_font);
-  
+
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_symbol_layer);
@@ -105,66 +105,81 @@ static void main_window_unload(Window *window) {
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
   update_time();
-  
+
   // Get weather update every 15 minutes
   //if(tick_time->tm_min % 15 == 0)
-     fetch_weather();  
+  fetch_weather();  
 }
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  static char temperature_buffer[32];
-  static char conditions_buffer[32];
-  static char weather_layer_buffer[32];
-  static char symbol_buffer[32];
-  static char price_buffer[32];
-  static char stock_layer_buffer[32];
-  
-   APP_LOG(APP_LOG_LEVEL_ERROR, "----got msg------");
+  static char temperature_buffer[99];
+  static char conditions_buffer[99];
+  static char weather_layer_buffer[99];
+  static char symbol_buffer[99];
+  static char price_buffer[99];
+  static char stock_layer_buffer[99];
+
+  APP_LOG(APP_LOG_LEVEL_ERROR, "----got msg------");
   // Read first item
   Tuple *t = dict_read_first(iterator);
 
-  temperature_buffer[0]=0;
-  conditions_buffer[0]=0;
-  
+  *temperature_buffer=0;
+  *conditions_buffer=0;
+  *symbol_buffer=0;
+  *price_buffer=0;
+
+
   while(t != NULL) {
-        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d", (int)t->key);
-      
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d", (int)t->key);
+
     switch(t->key) {
       case KEY_TEMPERATURE:
-        APP_LOG(APP_LOG_LEVEL_ERROR, "setting temp with %s", t->value->cstring);
-        //snprintf(temperature_buffer, sizeof(temperature_buffer), "%s\u00B0", t->value->cstring);
-        break;
+      APP_LOG(APP_LOG_LEVEL_ERROR, "setting temp with %s, %d", t->value->cstring, strlen(t->value->cstring));
+      snprintf(temperature_buffer, sizeof(temperature_buffer), "%s\u00B0", t->value->cstring);
+      APP_LOG(APP_LOG_LEVEL_ERROR, "temp bufer is %s, %d", temperature_buffer, strlen(temperature_buffer));
+      break;
+
       case KEY_CONDITIONS:
-        APP_LOG(APP_LOG_LEVEL_ERROR, "setting conditions with %s", t->value->cstring);
-        //snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-        break;
+      APP_LOG(APP_LOG_LEVEL_ERROR, "setting conditions with %s, %d", t->value->cstring, strlen(t->value->cstring));
+      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+      break;
+
       case KEY_CITY:
-        APP_LOG(APP_LOG_LEVEL_ERROR, "setting city with %s", t->value->cstring);
-        text_layer_set_text(s_weather_city_layer, t->value->cstring);    
-        break;
+      APP_LOG(APP_LOG_LEVEL_ERROR, "setting city with %s, %d", t->value->cstring, strlen(t->value->cstring));
+      text_layer_set_text(s_weather_city_layer, t->value->cstring);    
+      break;
+
       case KEY_SYMBOL:
-        APP_LOG(APP_LOG_LEVEL_ERROR, "setting symbol with %s", t->value->cstring);
-        text_layer_set_text(s_symbol_layer, t->value->cstring);  
-        break;
+      APP_LOG(APP_LOG_LEVEL_ERROR, "setting symbol with %s, %d", t->value->cstring, strlen(t->value->cstring));
+      snprintf(symbol_buffer, sizeof(symbol_buffer), "%s", t->value->cstring);
+      break;
+
       case KEY_PRICE:
-        APP_LOG(APP_LOG_LEVEL_ERROR, "setting price with %s", t->value->cstring);
-        snprintf(price_buffer, sizeof(price_buffer), "%s", t->value->cstring);
-        break;
+      APP_LOG(APP_LOG_LEVEL_ERROR, "setting price with %s, %d", t->value->cstring, strlen(t->value->cstring));
+      snprintf(price_buffer, sizeof(price_buffer), "%s", t->value->cstring);
+      break;
+
       default:
-        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-        break;
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
     }
 
     // Look for next item
     t = dict_read_next(iterator);
   }
-  
+
   if (*temperature_buffer && *conditions_buffer)
   {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "setting weather with %s", temperature_buffer);
-
     snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    APP_LOG(APP_LOG_LEVEL_ERROR, "setting weather with [%s]", weather_layer_buffer);
     text_layer_set_text(s_weather_layer, weather_layer_buffer);  
+  }
+
+  if (*symbol_buffer && *price_buffer)
+  {
+    snprintf(stock_layer_buffer, sizeof(stock_layer_buffer), "%s: %s (0)", symbol_buffer, price_buffer);
+    APP_LOG(APP_LOG_LEVEL_ERROR, "setting stock to [%s]", stock_layer_buffer);
+    text_layer_set_text(s_symbol_layer, stock_layer_buffer);  
   }
 }
 
@@ -179,20 +194,20 @@ static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResul
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
-  
+
 static void init() {
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  
+
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
   app_message_register_inbox_dropped(inbox_dropped_callback);
   app_message_register_outbox_failed(outbox_failed_callback);
   app_message_register_outbox_sent(outbox_sent_callback);
-  
+
   // Open AppMessage
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-  
+
   // Create main Window element and assign to pointer
   s_main_window = window_create();
 
--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,9 @@
-// We use the fake "PBL" symbol as default
-var defaultSymbol = "DIS";
-var symbol = defaultSymbol;
+var _temperature = "N/A";
+var _conditions = "N/A";
+var _city = "locating...";
+var _symbol = "DIS";
+var _price = "retrieving...";
+var _change = "0";
 
 var xhrRequest = function (url, type, callback) {
   var xhr = new XMLHttpRequest();
@@ -11,40 +14,46 @@ var xhrRequest = function (url, type, callback) {
   xhr.send();
 };
 
+function SendMessage(method)
+{
+  Pebble.sendAppMessage(
+    {
+      'KEY_TEMPERATURE': _temperature,
+      'KEY_CONDITIONS': _conditions,
+      'KEY_CITY': _city,
+      'KEY_SYMBOL': _symbol,
+      'KEY_PRICE': _price
+    },
+    function(e) { console.log(method + ' sent to Pebble successfully!'); },
+    function(e) { console.log(method + ' error sending to Pebble!'); }
+  );
+}
+
 function locationSuccess(pos)
 {
   var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude;
 
   xhrRequest(url, 'GET', 
-    function(responseText) {
-      console.log(responseText);
-      
-      var json = JSON.parse(responseText);
-      var temperature = Math.round(json.main.temp * (9.0/5.0) - 459.67);
-      var conditions = json.weather[0].main;      
-      var city = json.name;
-      
-      Pebble.sendAppMessage(
-        {
-          'KEY_TEMPERATURE': temperature.toString(),
-          'KEY_CONDITIONS': conditions,
-          'KEY_CITY': city
-        },
-        function(e) { console.log('Weather info sent to Pebble successfully!'); },
-        function(e) { console.log('Error sending weather info to Pebble!'); }
-      );
-    }      
-  );
+             function(responseText) {
+               console.log(responseText);
+
+               var json = JSON.parse(responseText);
+               _temperature = Math.round(json.main.temp * (9.0/5.0) - 459.67).toString();
+               _conditions = json.weather[0].main;      
+               _city = json.name;
+
+               SendMessage("weather");
+             }      
+            );
 }
 
 function locationError(err)
 { 
+  _temperature = "N/A";
+  _conditions = "N/A";
+  _city = "location N/A";
   console.warn('location error (' + err.code + '): ' + err.message); 
-  Pebble.sendAppMessage({ 
-    'KEY_TEMPERATURE': 'N/A',
-    'KEY_CONDITIONS': 'N/A',
-    'KEY_CITY': 'Loc unavailable'
-    }); 
+  SendMessage("location error");
 } 
 
 function getWeather() {
@@ -57,126 +66,64 @@ function getWeather() {
 
 function getQuotes()
 {
-      // Fetch saved symbol from local storage (using
-    // standard localStorage webAPI)
-    symbol = localStorage.getItem("symbol");
-    if (!symbol) {
-      symbol = "DIS";
-    }    
-    console.log('FETCH ' + symbol);
-    FetchQuote(symbol);
+  var symbol = localStorage.getItem("symbol");
+
+  if (!symbol)
+    symbol = "DIS";
+
+  console.log('FETCH ' + symbol);
+  FetchQuote(symbol);
 }
 
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready', 
-  function(e) {
-    console.log('PebbleKit JS ready!');
+                        function(e) {
+                          console.log('PebbleKit JS ready!');
 
-    getWeather();
-    getQuotes();
-  }
-);
+                          getWeather();
+                          getQuotes();
+                        }
+                       );
 
 // Listen for when an AppMessage is received
 Pebble.addEventListener('appmessage',
-  function(e) {
-    console.log('AppMessage received!');
-    
-    getWeather();
-    getQuotes();
-  }                     
-);
+                        function(e) {
+                          console.log('AppMessage received!');
+
+                          getWeather();
+                          getQuotes();
+                        }                     
+                       );
 
 ////////////////////////////////////////////////////////////////
 // STOCK STUFF
 ////////////////////////////////////////////////////////////////
 
-function FetchQuote(symbol) {
+function FetchQuote(symbol)
+{
+  _symbol = symbol;
   var url = "http://dev.markitondemand.com/Api/Quote/json?symbol=" + symbol;
 
   xhrRequest(url, 'GET', 
-    function(responseText) {
-      console.log(responseText);
-      
-        var response = JSON.parse(responseText);
-        if (response.Message)
-        {
-          // the markitondemand API sends a response with a Message
-          // field when the symbol is not found
-          Pebble.sendAppMessage(
-            { 
-              'KEY_SYMBOL': symbol,
-              'KEY_PRICE': 'N/A'
-            });
-        }
-        if (response.Data)
-        {
-          var price = response.Data.LastPrice;
-          var change = response.Data.Change.toString();
-          if (response.Data.Change > 0)
-            change = "+" + response.Data.Change;         
-          
-          Pebble.sendAppMessage(            
-            { 
-              'KEY_SYMBOL': symbol + ": $" + price + " (" + change + ")",
-              'KEY_PRICE': "$" + price + " (" + change + ")"
-            },
-            function(e) { console.log('Sent stock info to Pebble!'); },
-            function(e) { console.log('Error sending stock info to Pebble!'); }
-      );
-        }      }
-  );
-}
+             function(responseText)
+             {
+               console.log(responseText);
 
-// Fetch stock data for a given stock symbol (NYSE or NASDAQ only) from markitondemand.com
-// & send the stock price back to the watch via app message
-// API documentation at http://dev.markitondemand.com/#doc
-function fetchStockQuote(symbol, isInitMsg) {
-  var response;
-  var req = new XMLHttpRequest();
-  // build the GET request
-  req.open('GET', "http://dev.markitondemand.com/Api/Quote/json?" +
-    "symbol=" + symbol, true);
-  req.onload = function(e) {
-    
-        console.log('req state ' + req.readyState);
+               var response = JSON.parse(responseText);
+               if (response.Message)
+               {
+                 _price = "N/A";
+                 SendMessage("price error");
+               }
+               if (response.Data)
+               {
+                 _price = response.Data.LastPrice.toFixed(2);
+                 _change = response.Data.Change.toFixed(2);
+                 if (response.Data.Change > 0)
+                   _change = "+" + _change;         
 
-    if (req.readyState == 4) {
-      // 200 - HTTP OK
-      if(req.status == 200) {
-        console.log(req.responseText);
-        response = JSON.parse(req.responseText);
-        var price;
-        if (response.Message) {
-          // the markitondemand API sends a response with a Message
-          // field when the symbol is not found
-          Pebble.sendAppMessage(
-            { 
-              'KEY_SYMBOL': symbol,
-              'KEY_PRICE': 'N/A'
-            });
-        }
-        if (response.Data) {
-          // data found, look for LastPrice
-          price = response.Data.LastPrice;
-          console.log(price);
-
-          var msg = {};
-          if (isInitMsg) {
-            msg.init = true;
-            msg.symbol = symbol;
-          }
-          msg.price = "$" + price.toString();
-          Pebble.sendAppMessage(            
-            { 
-              'KEY_SYMBOL': symbol,
-              'KEY_PRICE': "$" + price.toString()
-            });
-        }
-      } else {
-        console.log("Request returned error code " + req.status.toString());
-      }
-    }
-  };
-  req.send(null);
+                 SendMessage("quote");
+               }      
+             }
+            );
 }
